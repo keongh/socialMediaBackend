35c1753a681ba32176594053e762ab6b86c26d1b
keon <kghadamyari@gmail.com>
2020-08-29 14:46:46 -0400

    Add follow and unfollow functionality

--- src/main/java/com/example/socialmedia/models/User.java
+++ src/main/java/com/example/socialmedia/models/User.java
@@ -1,16 +1,15 @@
 package com.example.socialmedia.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Entity
@@ -27,11 +26,18 @@ public class User {
     private String role = "USER";
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     @JsonSerialize(using = ToStringSerializer.class)
     private final LocalDateTime registrationDate = LocalDateTime.now();
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    List<User> followers;
+
+    @ManyToMany(mappedBy = "followers")
+    @JsonIgnore
+    List<User> following;
 
     public User() {
     }
