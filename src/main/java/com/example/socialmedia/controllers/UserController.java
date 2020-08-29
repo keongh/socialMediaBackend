9ba55fcc1d3339a52bb24a0fd82f53371105c628
keon <kghadamyari@gmail.com>
2020-08-28 20:24:32 -0400

    Move @CrossOrigin above @RestController?

--- src/main/java/com/example/socialmedia/controllers/UserController.java
+++ src/main/java/com/example/socialmedia/controllers/UserController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@RestController
 @CrossOrigin(origins = "*", allowCredentials = "true")
+@RestController
 public class UserController {
 
     private final UserService userService;
